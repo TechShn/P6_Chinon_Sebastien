4c0796e1897441bf3099824bbc93fe979a04e417
Sebastien Chinon <sebastien.chinon@hotmail.com>
2022-08-08 10:38:56 +0200

    08/08/2022

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Thing = require('./models/thing')
 
 const app = express();
+
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://TechShn_P6:53525770@cluster0.l4zrhit.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -11,11 +21,26 @@ app.use((req, res, next) => {
 
 
 app.post('/api/auth/signup', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({message: 'objet crée !'});
+    const thing = new Thing({
+      ...req.body
+    });
+    thing.save()
+    .then(() => res.status(201).json({ message: 'Objet crée !'}))
+    .then(console.log(thing))
+    .catch(error => res.status(400).json({ error }));
 });
 
-app.get('/api/auth/signup', (req, res, next) => {
+app.post('/api/auth/login', (req, res, next) => {
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+  .then(() => res.status(201).json({ message: 'Objet crée !'}))
+  .then(console.log(thing))
+  .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/auth/login', (req, res, next) => {
   const stuff = [
     {
       _id: 'oeihfzeoi',
